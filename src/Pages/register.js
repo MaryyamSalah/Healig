e5c2bcd7c4ead26ea6f77e12d1447923e2e0ae29
MaryyamSalah <maryyamsalah20@gmail.com>
2023-02-15 22:06:40 +0300

    routing the pages

---
+++ src/Pages/register.js
@@ -0,0 +1,70 @@
+import React ,{useState,useEffect} from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+import {useHistory ,Link} from 'react-router-dom'
+const Register = () => {
+    // const [state,useState]=useState({
+    //     email:'',
+    //     password:'',
+    //   })
+    //   const {email,password}=state;
+    const handleGoogleSignIn= ()=>{}
+    const handleFBSignIn= ()=>{}
+    
+    const handleSubmit= ()=>{}
+    const handleChange= ()=>{}
+    
+      
+      
+      
+     
+      
+    return (
+        <div>
+          <div id='logreg-from'>
+            <form className='form-signin' onSubmit={handleSubmit}>
+               <h1>signin</h1>
+               <div>
+                 <button type='button' onClick={handleGoogleSignIn}>
+                  <span>
+                    <i className='fab fa-google-plus-g '>sign in with google</i>
+                  </span>
+                 </button>
+                 <button type='button' onClick={handleFBSignIn}>
+                  <span>
+                    <i className='fab fa-google-plus-g '>sign in with Facebook</i>
+                  </span>
+                 </button>
+                 <p>or</p>
+                 <input 
+                 type='email'
+                id='inputEmail'
+                placeholder='EmailAddress'
+                name='email'
+                onChange={handleChange}
+                // value={email}
+                required
+                 />
+                 <input 
+                 type='password'
+                id='inputpassword'
+                placeholder='Password'
+                name='password'
+                onChange={handleChange}
+                // value={password}
+                required
+                 />
+                 <button type='submit'>sign In</button>
+                 <hr/>
+                 <p>Don't have account</p>
+                 <Link to='/register'> 
+                  <button type='submit'>sign Up new account</button>
+                 </Link>
+               </div>
+            </form>
+           </div>
+        </div>
+      )
+    }
+    
+    export default Register
+    
