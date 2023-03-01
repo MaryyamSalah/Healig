82670e0f6c30b4e8ad41f53a749f8b9e1a030f6f
MaryyamSalah <maryyamslah20@gmail.com>
2023-03-01 15:01:58 +0300

    Private Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import { Router,Route, Routes } from 'react-router-dom';
 import Home from './Pages/home';
 import Login from './Pages/login';
 import Register from './Pages/register';
+import UserRoute from './Components/UserRoute';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
+     
     <div className="App">
-     <Routes>
-     <Route path="/" element={<Home />} />
+    <Routes>
+    <Route exact path='/' element={<UserRoute />} >
+    <Route exact path='/' element={<Home />}/>
+          </Route>
      <Route path="/login" element={<Login />} />
      <Route path="/register" element={<Register />} />
 
 
       </Routes>
     </div>
-
-    </BrowserRouter>
+   
+    </Router>
   );
 }
 
